1821f077ec638a367fa918c8af12c9422601fa09
Sanket Chopade <sanketchopade@gmail.com>
2015-04-02 20:27:56 +0800

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mytodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mytodoApp
+ */
+angular.module('mytodoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.todos = [
+      'Item1',
+      'Item2',
+      'Item3'
+    ];
+    $scope.addTodo = function () {
+    	$scope.todos.push($scope.todo);
+    	$scopr.todo = '';
+    };
+    $scope.removeTodo = function (index) {
+    	$scope.todos.splice(index, 1);
+    };
+  });
